dae5b9c675052ac1a802163b70d45ac84e3c16ad
ForisKuang <Forisk@uw.edu>
2015-10-18 05:48:17 -0700

    Timer editted as well as timer input

--- app/src/main/java/com/example/kfc36_000/napp/TimerInput.java
+++ app/src/main/java/com/example/kfc36_000/napp/TimerInput.java
@@ -1,6 +1,7 @@
 package com.example.kfc36_000.napp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class TimerInput extends AppCompatActivity {
@@ -19,25 +21,23 @@ public class TimerInput extends AppCompatActivity {
         setContentView(R.layout.activity_timer_input);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final EditText timerText = (EditText)findViewById(R.id.editText);
-        Button ok = (Button) findViewById(R.id.okButton);
 
-        ok.setOnClickListener(new View.OnClickListener() {
+        final EditText timerText = (EditText)findViewById(R.id.timerText);
+        final EditText timeToSleep = (EditText) findViewById(R.id.timeToSleepText);
+        ImageButton start = (ImageButton) findViewById(R.id.startButton);
+        Typeface roboto = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Bold.ttf");
+        timerText.setTypeface(roboto);
+        timeToSleep.setTypeface(roboto);
+
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String time = timerText.getText().toString();
+                String timeTill = timeToSleep.getText().toString();
                 Intent i = new Intent(TimerInput.this, MainActivity.class);
-                i.putExtra("timeValue",time);
+                i.putExtra("timeForSleep",time);
+                i.putExtra("timeTillSleep", timeTill);
                 startActivity(i);
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
