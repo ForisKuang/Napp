97a0ee244affd3a7c56d435db1bad42cbf63a13b
ForisKuang <Forisk@uw.edu>
2015-10-17 21:13:14 -0700

    Got TimerInput running

--- app/src/main/java/com/example/kfc36_000/napp/TimerInput.java
+++ app/src/main/java/com/example/kfc36_000/napp/TimerInput.java
@@ -1,11 +1,15 @@
 package com.example.kfc36_000.napp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TimerInput extends AppCompatActivity {
 
@@ -15,6 +19,18 @@ public class TimerInput extends AppCompatActivity {
         setContentView(R.layout.activity_timer_input);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final EditText timerText = (EditText)findViewById(R.id.editText);
+        Button ok = (Button) findViewById(R.id.okButton);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String time = timerText.getText().toString();
+                Intent i = new Intent(TimerInput.this, MainActivity.class);
+                i.putExtra("timeValue",time);
+                startActivity(i);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
