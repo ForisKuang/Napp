fa9a621b578f663b7e79f9abe0810e61bc0038c4
ForisKuang <Forisk@uw.edu>
2015-10-18 06:44:05 -0700

    added IDs to feelings images and created button values as well as onclicklisteners for them TODO: Implement algorithm to shift values

--- app/src/main/java/com/example/kfc36_000/napp/FeelingsActivity.java
+++ app/src/main/java/com/example/kfc36_000/napp/FeelingsActivity.java
@@ -1,11 +1,14 @@
 package com.example.kfc36_000.napp;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class FeelingsActivity extends AppCompatActivity {
 
@@ -16,14 +19,50 @@ public class FeelingsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ImageButton groggy = (ImageButton) findViewById(R.id.groggy);
+        ImageButton refreshed = (ImageButton) findViewById(R.id.refreshed);
+        ImageButton feelBetter = (ImageButton) findViewById(R.id.feelBetter);
+        ImageButton disoriented = (ImageButton) findViewById(R.id.disoriented);
+
+        groggy.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                connectBackToHome();
             }
         });
+
+        refreshed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                connectBackToHome();
+            }
+        });
+
+        feelBetter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                connectBackToHome();
+            }
+        });
+
+        disoriented.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                connectBackToHome();
+            }
+        });
+
+    }
+
+    protected void connectBackToHome() {
+        Intent intent = new Intent(FeelingsActivity.this,TimerInput.class);
+        startActivity(intent);
+        finish();
     }
 
 }
